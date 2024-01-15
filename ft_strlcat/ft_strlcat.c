18312d3a11dd840d9000ce74436bf423d7446dc4
frafont42 <fra.fon11@gmail.com>
2024-01-15 12:59:53 +0100

    5th

--- ft_strlcat/ft_strlcat.c
+++ ft_strlcat/ft_strlcat.c
@@ -2,7 +2,8 @@ size_t ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t dest_len;
 	size_t src_len;
-	int i;
+	size_t i;
+	size_t tot_len;
 
 	i = 0;
 	dest_len = 0;
@@ -11,14 +12,16 @@ size_t ft_strlcat(char *dest, const char *src, size_t size)
 	{
 		dest_len++;
 	}
-	while(src_len < dest_len - size - 1 && src[src_len] != '\0')
+	while(src[src_len] != '\0')
 	{
 		src_len++;
 	}
-	while(src[i] != '\0')
+	tot_len = src_len + dest_len;
+	while(src[i] != '\0' && dest_len + i < size - i)
 	{
 		dest[dest_len + i] = src[i];
 		i++;
 	}
-	return (dest_len + src _len);
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
