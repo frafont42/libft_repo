eefbd743bd30802b3b578dd178b4cac4193a4e04
frafont42 <fra.fon11@gmail.com>
2024-02-07 21:22:22 +0100

    14th

--- part2/ft_itoa/ft_itoa.c
+++ part2/ft_itoa/ft_itoa.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int nlen(int n)
 {
 	int i;
@@ -33,30 +36,35 @@ char *ft_itoa(int n)
 {
 	char *str;
 	int i;
+	long long nb;
 
 	str = (char *)malloc(sizeof(char) * (nlen(n) + 1));
 	i = nlen(n);
-	if (n < 0)
+	if (n != -2147483648LL)	
+		nb = (long long)n;
+	else
+		nb = -2147483648;
+	if (nb < 0)
 	{
-		n *= -1;
+		nb *= -1;
 		str[i] = '\0';
 		i--;
 		while(n != 0)
 		{
-			str[i] = n % 10 + 48;
-			n /= 10;
+			str[i] = nb % 10 + 48;
+			nb /= 10;
 			i--;
 		}
 		str[i] = '-';
 	}
-	else if (n > 0)
+	else if (nb > 0)
 	{
 		str[i] = '\0';
 		i--;
 		while(n != 0)
 		{
-			str[i] = n % 10 + 48;
-			n /= 10;
+			str[i] = nb % 10 + 48;
+			nb /= 10;
 			i--;
 		}
 	}
@@ -67,3 +75,16 @@ char *ft_itoa(int n)
 	}
 	return str;
 }
+
+int main() {
+    long long n = -2147483648LL;
+    char *result = ft_itoa(n);
+
+    if (result != NULL) {
+        printf("Stringa risultante: %s\n", result);
+        free(result);  // Ricordati di liberare la memoria
+    } else {
+        printf("Errore nell'allocazione di memoria.\n");
+    }
+
+    return 0;}
