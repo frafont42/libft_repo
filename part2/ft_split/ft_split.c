17edfcc01341847b9e60951fc49c6318a036ab9e
frafont42 <fra.fon11@gmail.com>
2024-02-07 21:20:02 +0100

    13th

--- part2/ft_split/ft_split.c
+++ part2/ft_split/ft_split.c
@@ -1,84 +1,109 @@
-unsigned int words_counter(const char *str, char a)
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static size_t	words_counter(const char *str, char c)
 {
-	unsigned int i;
-	unsigned int counter;
-	
+	unsigned int	i;
+	size_t		counter;
+
 	i = 0;
 	counter = 0;
+	while (str[i] == c)
+		i++;
+	if (str[i] == '\0')
+		return (counter);
 	while (str[i] != '\0')
 	{
-		if (str[i] == a)
+		counter++;
+		while (str[i] != '\0' && str[i] != c)
+			i++;
+		while (str[i] == c)
+			i++;
+		if (str[i] == '\0')
+			return (counter);
+	}
+}
+
+static size_t	*len_vector(const char *str, char c)
+{
+	size_t	*vector;
+	int	i;
+	size_t	counter;
+
+	i = 0;
+	vector = (size_t *)malloc(sizeof(size_t) * (1 + words_counter(str, c)));
+	vector[words_counter(str, c)] = '\0';
+	if (!vector)
+		return (NULL);
+	while (*str == c)
+		str++;
+	while (*str)
+	{
+		counter = 0;
+		while (*str && *str != c)
 		{
-			if (str[i + 1] == '\0')
-				break;
+			str++;
 			counter++;
 		}
-		i++;
+		vector[i++] = counter;
+		while (*str == c)
+			str++;
+		if (*str == '\0')
+			return (vector);
 	}
-	counter++;
-	return counter;
+	return (NULL);
 }
 
-static void ft_allocation(char **slots, char const *str, char h)
+char	**ft_split(const char *s, char c)
 {
-	int i;
-	int j;
-	int z;
+	char	**arr;
+	size_t	*v;
 
-	z = 0;
-	i = 0;
-	while (str[i] != '\0' && str[i] != h)
-	{
-		j = 0;
-		while(str[i] != h && str[i] != '\0')
-		{
-			i++;
-			j++;
-		}
-		slots[z] = (char *)malloc(sizeof(char) * (j + 1));
-		z++;
-		if (str[i] != '\0')
-			i++;
-		while (str[i] == h)
-			i++;
-	}
-}
-
-char **ft_split(char const *s, char c)
-{
-	char **arr;
-	int i;
-	int j;
-	int z;
-
-	i = 0;
-	z = 0;
-	arr = malloc(sizeof(char **) * (1 + words_counter(s, c)));
-	ft_allocation(arr, s, c);
-	while (s[i] != '\0')
-	{
-		j = 0;
-		while(s[i] == c)
-			i++;
-		if (s[i] == '\0')
-			break;
-		while (s[i] != c && s[i] != '\0')
-		{
-			arr[z][j] = s[i];
-			i++;
-			j++;
-		}
-		arr[z][j] = '\0';
-		if (s[i] != '\0')
-		{	
-			i++;
-			z++;
-		}
-	}
+	if (!s)
+		return (NULL);
+	v = len_vector(s, c);
+	arr = (char **)malloc(sizeof(char *) * words_counter(str, c));
 	if (!arr)
-		return 0;
-	z++;
-	j = 0;
-	arr[z] = NULL;
-	return arr;
+		return (NULL);
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		arr[i++] = ft_substr(s, 0, v[i++]);
+		while (*s && *s != c)
+			s++;
+		if (*s == '\0')
+		{
+			arr[i] = NULL;
+			return (arr);
+		}
+	}
+}
+
+
+
+
+
+
+
+int main() {
+    const char *test_string = "12345 123 12 1 1234567 1234567890";
+    char separator = ' ';
+
+    // Test della funzione len_vector
+    size_t *result = len_vector(test_string, separator);
+
+    if (result == NULL) {
+        printf("Errore: allocazione di memoria fallita\n");
+        return 1;
+    }
+    printf("Lunghezze delle parole separate da '%c':\n", separator);
+    for (size_t i = 0; result[i] != 0; i++) {
+        printf("Parola %zu: %zu caratteri\n", i + 1, result[i]);
+    }
+
+    free(result); // Ricordati di liberare la memoria allocata
+
+    return 0;
 }
